07d7fed53a37a57db5a80c3cb1e32c18ffe4411b
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-09-21 10:06:50 +0700

    add method show to get data by id

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -49,4 +49,14 @@ class PostController extends Controller
         //return response
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
+
+    // Get Data by Id
+    public function show($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //return single post as a resource
+        return new PostResource(true, 'Detail Data Post!', $post);
+    }
 }
