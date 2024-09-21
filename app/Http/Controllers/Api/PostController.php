f78f09c43f63f65ee017961ddf06335cd3859597
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-09-21 10:47:29 +0700

    add method update data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -23,40 +24,86 @@ class PostController extends Controller
     // Insert Data
     public function store(Request $request)
     {
-        //define validation rules
+        // Define validation rules
         $validator = Validator::make($request->all(), [
             'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'title'     => 'required',
             'content'   => 'required',
         ]);
 
-        //check if validation fails
+        // Check if validation fails
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        //upload image
+        // Upload image
         $image = $request->file('image');
         $image->storeAs('public/posts', $image->hashName());
 
-        //create post
+        // Create post
         $post = Post::create([
             'image'     => $image->hashName(),
             'title'     => $request->title,
             'content'   => $request->content,
         ]);
 
-        //return response
+        // Return response
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 
     // Get Data by Id
     public function show($id)
     {
-        //find post by ID
+        // Find post by ID
         $post = Post::find($id);
 
-        //return single post as a resource
+        // Return single post as a resource
         return new PostResource(true, 'Detail Data Post!', $post);
+    }
+
+    // Update Data
+    public function update(Request $request, $id)
+    {
+        // Define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Find post by ID
+        $post = Post::find($id);
+
+        // Check if image is not empty
+        if ($request->hasFile('image')) {
+
+            // Upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // Delete old image
+            Storage::delete('public/posts/' . basename($post->image));
+
+            // Update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        } else {
+
+            // Update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        }
+
+        // Return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
     }
 }
